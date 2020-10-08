8bef18aef5ef805f3e79ceae34207dc175bd4d66
Admin <admin@iMac-Admin.local>
2020-10-08 18:53:38 +0300

    primul commit

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+{
+    return $next($request)->header('Access-Control-Allow-Origin', '*')
+    ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+//         $response = $next($request);
+// $response->headers->set('Access-Control-Allow-Origin', '*');
+// $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+// $response->headers->set('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, X-Auth-Token');
+// $response->headers->set('Access-Control-Allow-Credentials',' true');
+// return $response;
+}
+}
